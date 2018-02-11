157057b7f69c9360d6344a13abf8fb2abdaec33d
Christina Thalayasingam <cthalayasingam@zaizi.com>
2018-02-11 20:24:06 +0530

    my changes

--- test/database.js
+++ test/database.js
@@ -1,40 +1,69 @@
-'use strict';
+process.env.NODE_ENV = 'test';
 
-var expect = require('chai').expect;
-var request = require('request');
+//let mongoose = require('mongoose');
+let baseUrl = 'http://localhost:3000';
+let Vegetables = require('../lib/vegetables.js');
 
-describe('vegetable api - database', function() {
-    var baseUrl = 'http://localhost:3000';
-    var server = require('../server.js');
-    var vegetables = require('../lib/vegetables.js');
+//let mongoose = require("mongoose");
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server').default;
 
-    before(function(done) {
-        server.start(done);
-    });
+let should = chai.should();
+let expect = chai.expect;
 
-    afterEach(function() {
-        vegetables.length = 0;
-    });
+chai.use(chaiHttp);
 
+describe('Vegetables', function() {
+    /*afterEach(function(done){
+        Vegetables.collection.drop();
+        done();
+      });*/
     describe('when POST /vegetable is requested', function() {
         it('should have the posted vegetable in the vegetables database', function() {
+            chai.request(server)
+            .post('/vegetable').send('egg plant')
+            .end(function(err, res) {
+                console.log(res);
+                res.should.have.status(200);
+                done();
+                chai.request(server)
+                .get('/vegetable')
+                .end(function(err, res) {
+                    res.should.have.status(200);
+                    expect(res).should.have.status(200);
+                    expect(res.body).should.be.a('array');
+                    expect(res.body).should.be.eql(1);
+                    done();
+                });
 
         });
     });
+});
 
     describe('when GET /vegetable is requested', function() {
-        it('should respond with all the vegetables in the vegetables database', function() {
+                it('should respond with all the vegetables in the vegetables database', function() {
+            chai.request(server)
+            .get('/vegetable')
+            .end(function(err, res) {
+                console.log(res.status);
+                expect(res).to.have.status(200);
+                expect(res.body).should.be.a('array');
+                expect(res.body).should.be.eql(0);
+                done();
+            });
 
         });
 
-        it('should respond with all vegetables upperCased if the query parameter \'upperCase\' is \'true\'', function() {
+        /*it('should respond with all vegetables upperCased if the query parameter \'upperCase\' is \'true\'', function() {
 
-        });
+        });*/
     });
 
-    describe('when DELETE /vegetable/:name is requested', function() {
+    /*describe('when DELETE /vegetable/:name is requested', function() {
         it('should respond with 200 and \'Successfully deleted: \' and the vegetable name', function() {
 
         });
-    });
+    });*/
 });
